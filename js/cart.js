cf37391fd973b7320e0bc3e48ea82a7f345695ca
pchabana <135026202+pchabana@users.noreply.github.com>
2023-06-07 05:08:40 -0400

    My updated file

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,283 @@
-function addCartItem(n,t,i){if(n==null||t==null||i==null)return!1;let e=document.createElement("tr");e.setAttribute("class","table_row");let r=document.createElement("td");r.setAttribute("class","column-1");let u=document.createElement("div");u.setAttribute("class","how-itemcart1");u.addEventListener("click",()=>removeCartItemFromView(i.slug,$(e)));let h=document.createElement("img");h.setAttribute("src","./images/products/"+i.slug+"-0.webp");h.setAttribute("alt","im�gen del art�culo");h.setAttribute("loading","lazy");u.appendChild(h);r.appendChild(u);e.appendChild(r);r=document.createElement("td");r.setAttribute("class","column-2");aElement=document.createElement("a");aElement.setAttribute("class","cl3");aElement.setAttribute("href","product.html?id="+i.slug);aElement.textContent=i.Label;r.appendChild(aElement);e.appendChild(r);r=document.createElement("td");r.setAttribute("class","column-3");r.textContent=toMoneyStr(i.Price);e.appendChild(r);r=document.createElement("td");r.setAttribute("class","column-4");u=document.createElement("div");u.setAttribute("class","wrap-num-product flex-w m-l-auto m-r-0");let o=document.createElement("div");o.setAttribute("class","btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m");let s=document.createElement("i");s.setAttribute("class","fs-16 zmdi zmdi-minus");o.appendChild(s);u.appendChild(o);let f=document.createElement("input");return f.setAttribute("class","mtext-104 cl3 txt-center num-product"),f.setAttribute("type","number"),f.setAttribute("name","num-"+i.slug),f.setAttribute("value",t.qty),f.setAttribute("min",0),f.setAttribute("max",99),f.setAttribute("price",i.Price),f.setAttribute("label",i.Label),f.addEventListener("input",()=>updateCartTotals()),u.appendChild(f),o=document.createElement("div"),o.setAttribute("class","btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m"),s=document.createElement("i"),s.setAttribute("class","fs-16 zmdi zmdi-plus"),o.appendChild(s),u.appendChild(o),r.appendChild(u),e.appendChild(r),r=document.createElement("td"),r.setAttribute("class","column-5"),r.textContent=toMoneyStr(i.Price*t.qty),e.appendChild(r),n.append(e),!0}function updateCart(){let i=$(".table-shopping-cart");i.empty();let t=document.createElement("tr");t.setAttribute("class","table_head");let n=document.createElement("th");n.setAttribute("class","column-1");n.textContent=spanishFormat("Articulo");t.appendChild(n);n=document.createElement("th");n.setAttribute("class","column-2");t.appendChild(n);n=document.createElement("th");n.setAttribute("class","column-3");n.textContent="Precio";t.appendChild(n);n=document.createElement("th");n.setAttribute("class","column-4");n.textContent="Cantidad";t.appendChild(n);n=document.createElement("th");n.setAttribute("class","column-5");n.textContent="Total";t.appendChild(n);i.append(t);let r=getCart(),u=[];for(let n of r.items)addCartItem(i,n,productSlugs[n.productId])||u.push(n.productId);for(let n of u)removeCartItem(r,n);$(".btn-num-product-down").on("click",function(){let n=$(this).next();var t=Number(n.val());t>0&&(n.val(t-1).change(),updateCartTotals())});$(".btn-num-product-up").on("click",function(){let n=$(this).prev();var t=Number(n.val());t<99&&(n.val(t+1).change(),updateCartTotals())});updateCartTotals(!1)}function clearCart(){let n=getCart();n.items=[];addStorage("cart",n);updateCart()}function updateCartTotals(n=true){let t={items:[]},i=0;$(".num-product").each(function(n,r){let e=$(r).attr("name").substring(4),u=$(r).val(),o=$(r).attr("price"),f=o*u;t.items.push({productId:e,qty:u});let s=$(this).parent().parent(),h=s.next(),c=toMoneyStr(f);h.text(c);i+=f});let r=$("span.mtext-110.cl2"),u=toMoneyStr(i);r.text(u);n&&addStorage("cart",t)}function removeCartItemFromView(n,t){let i=getCart();t.remove();removeCartItem(i,n)}function removeCartItem(n,t){n.items=n.items.filter(function(n){return n.productId!==t});addStorage("cart",n);updateCartTotals(!1)}function sendOrder(){let n="Hola, quiero esta orden:\n";n+="------------------------------\n";let t=0;$(".num-product").each(function(i,r){let e=$(r).attr("label"),u=$(r).val();if(!(u<=0)){let f=$(r).attr("price");n+="- "+u+"x $"+f+" "+e+"\n";let o=f*u;t+=o}});n+="\nTotal: $"+t;let i="https://wa.me/+"+contactCell+"?text="+encodeURIComponent(n);window.open(i)}var productSlugs=[];(function(n){"use strict";n.getJSON("./data/products-index.json",function(t){if(t!=null){productSlugs=[];for(let n in t){let i=t[n];for(let n in i){let t=i[n];for(let n of t){let t=ToSlug(n.Label);n.slug=t;productSlugs[t]=n}}}updateCart();n("button.pointer").on("click",function(n){n.preventDefault();sendOrder()});n(".clearcart").on("click",function(){clearCart()})}});window.addEventListener("storage",function(n){n.key==="cart"&&updateCart()})})(jQuery)
+var productSlugs = [];
+
+(function ($) {
+    "use strict";
+
+    $.getJSON("./data/products-index.json", function (data) {
+
+        if (data == null)
+            return;
+        
+        productSlugs = [];
+        for (let categoryKey in data) {
+            let category = data[categoryKey];
+            for (let subCategoryKey in category) {
+                let subcategory = category[subCategoryKey];
+                for (let product of subcategory) {
+                    let slug = ToSlug(product.Label);
+                    product.slug = slug;
+                    productSlugs[slug] = product;
+                }
+            }
+        }
+
+        updateCart(); 
+
+        $('button.pointer').on('click', function (event) {
+            event.preventDefault();
+            sendOrder();   
+        });
+
+        $('.clearcart').on('click', function (event) {           
+            clearCart();
+        });
+    });
+
+    window.addEventListener('storage', function (event) {
+        if (event.key === 'cart')
+            updateCart();
+    });
+})(jQuery);
+
+function addCartItem($container, item, product) {
+
+    if ($container == null || item == null || product == null)
+        return false;
+
+    let trElement = document.createElement("tr");
+    trElement.setAttribute("class", "table_row");
+
+    let tdElement = document.createElement("td");
+    tdElement.setAttribute("class", "column-1");
+
+    let divElement = document.createElement("div");
+    divElement.setAttribute("class", "how-itemcart1");
+    divElement.addEventListener("click", () => removeCartItemFromView(product.slug, $(trElement)));
+
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", "./images/products/" + product.slug + "-0.webp");
+    imgElement.setAttribute("alt", "im�gen del art�culo");
+    imgElement.setAttribute("loading", "lazy");
+
+    divElement.appendChild(imgElement);
+
+    tdElement.appendChild(divElement);
+
+    trElement.appendChild(tdElement);
+
+    tdElement = document.createElement("td");
+    tdElement.setAttribute("class", "column-2");
+
+    aElement = document.createElement("a");
+    aElement.setAttribute("class", "cl3");
+    aElement.setAttribute("href", "product.html?id=" + product.slug);
+    aElement.textContent = product.Label;
+
+    tdElement.appendChild(aElement);
+
+    trElement.appendChild(tdElement);
+
+
+    tdElement = document.createElement("td");
+    tdElement.setAttribute("class", "column-3");
+    tdElement.textContent = toMoneyStr(product.Price);
+
+    trElement.appendChild(tdElement);
+
+    tdElement = document.createElement("td");
+    tdElement.setAttribute("class", "column-4");
+
+    divElement = document.createElement("div");
+    divElement.setAttribute("class", "wrap-num-product flex-w m-l-auto m-r-0");
+
+    let div2Element = document.createElement("div");
+    div2Element.setAttribute("class", "btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m");
+    
+    let iElement = document.createElement("i");
+    iElement.setAttribute("class", "fs-16 zmdi zmdi-minus");
+
+    div2Element.appendChild(iElement);
+
+    divElement.appendChild(div2Element);
+
+    let inputElement = document.createElement("input");
+    inputElement.setAttribute("class", "mtext-104 cl3 txt-center num-product");
+    inputElement.setAttribute("type", "number");
+    inputElement.setAttribute("name", "num-" + product.slug);
+    inputElement.setAttribute("value", item.qty);
+    inputElement.setAttribute("min", 0);
+    inputElement.setAttribute("max", 99);
+    inputElement.setAttribute("price", product.Price);
+    inputElement.setAttribute("label", product.Label);
+    
+    inputElement.addEventListener("input", () => updateCartTotals());
+
+    divElement.appendChild(inputElement);
+
+
+    div2Element = document.createElement("div");
+    div2Element.setAttribute("class", "btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m");
+
+    iElement = document.createElement("i");
+    iElement.setAttribute("class", "fs-16 zmdi zmdi-plus");
+
+    div2Element.appendChild(iElement);
+  
+    divElement.appendChild(div2Element);
+
+    tdElement.appendChild(divElement);
+
+    trElement.appendChild(tdElement);
+
+    tdElement = document.createElement("td");
+    tdElement.setAttribute("class", "column-5");
+    tdElement.textContent = toMoneyStr(product.Price * item.qty);
+
+    trElement.appendChild(tdElement);
+
+    $container.append(trElement);
+
+    return true;
+}
+
+function updateCart() {
+
+    let $container = $('.table-shopping-cart');
+    $container.empty();
+
+    let trElement = document.createElement("tr");
+    trElement.setAttribute("class", "table_head");
+
+    let thElement = document.createElement("th");
+    thElement.setAttribute("class", "column-1");
+    thElement.textContent = spanishFormat("Articulo");
+    trElement.appendChild(thElement);
+
+    thElement = document.createElement("th");
+    thElement.setAttribute("class", "column-2");
+    trElement.appendChild(thElement);
+
+    thElement = document.createElement("th");
+    thElement.setAttribute("class", "column-3");
+    thElement.textContent = "Precio";
+    trElement.appendChild(thElement);
+
+    thElement = document.createElement("th");
+    thElement.setAttribute("class", "column-4");
+    thElement.textContent = "Cantidad";
+    trElement.appendChild(thElement);
+
+    thElement = document.createElement("th");
+    thElement.setAttribute("class", "column-5");
+    thElement.textContent = "Total";
+    trElement.appendChild(thElement);
+
+    $container.append(trElement);
+
+    let cart = getCart();
+    let toRemove = [];
+    for (let item of cart.items) 
+        if (!addCartItem($container, item, productSlugs[item.productId]))
+            toRemove.push(item.productId);        
+
+    for (let slug of toRemove)
+        removeCartItem(cart, slug);
+            
+    $('.btn-num-product-down').on('click', function () {
+        let input = $(this).next();
+        var numProduct = Number(input.val());
+        if (numProduct > 0) {
+            input.val(numProduct - 1).change();
+            updateCartTotals();
+        }
+    });
+
+    $('.btn-num-product-up').on('click', function () {
+        let input = $(this).prev();
+        var numProduct = Number(input.val());
+        if (numProduct < 99) {
+            input.val(numProduct + 1).change();
+            updateCartTotals();
+        }
+    });
+
+    updateCartTotals(false);
+}
+
+function clearCart() {
+    let cart = getCart();  
+    cart.items = [];  
+    addStorage('cart', cart);
+    updateCart();
+}
+
+function updateCartTotals(autosave = true) {
+
+    let cart = { items: [] };
+    let ttPrice = 0;
+    $('.num-product').each(function (index, element) {
+        
+        let slug = $(element).attr('name').substring(4);;
+        let qty = $(element).val();
+        let price = $(element).attr('price');
+        let tPrice = price * qty;
+
+        cart.items.push({ productId: slug, qty: qty });
+        
+        let parentTd = $(this).parent().parent();
+        let tPriceTd = parentTd.next();
+        let tPriceStr = toMoneyStr(tPrice);
+        tPriceTd.text(tPriceStr);
+
+        ttPrice += tPrice;
+    });
+
+    let mySpan = $('span.mtext-110.cl2');
+    let ttPriceStr = toMoneyStr(ttPrice);
+    mySpan.text(ttPriceStr);
+
+    if (autosave)
+        addStorage('cart', cart);
+}
+
+function removeCartItemFromView(productSlug, $trElement) {
+    let cart = getCart();  
+    $trElement.remove();
+    removeCartItem(cart, productSlug);
+}
+
+function removeCartItem(cart, productSlug) {
+    cart.items = cart.items.filter(function (item) {
+        return item.productId !== productSlug;
+    });
+
+    addStorage('cart', cart);
+    updateCartTotals(false);
+}
+
+function sendOrder() {
+    let message = "Hola, quiero esta orden:\n";
+    message += "------------------------------\n";
+    let total = 0;
+
+    $('.num-product').each(function (index, element) {
+
+        let label = $(element).attr('label');
+        let qty = $(element).val();
+
+        if (qty <= 0)
+            return;
+
+        let price = $(element).attr('price');
+
+        message += '- ' + qty + 'x $' + price + ' ' + label + '\n';
+        
+        let tPrice = price * qty;
+
+        total += tPrice;
+    });
+
+    message += "\nTotal: $" + total;    
+    let enlace = "https://wa.me/+" + contactCell + "?text=" + encodeURIComponent(message);
+    window.open(enlace);
+}
