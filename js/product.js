f2b82170e3e07c8e9a0c3c8805c2cd44bf8d434e
pchabana <135026202+pchabana@users.noreply.github.com>
2023-07-09 10:26:18 -0400

    My updated file

--- js/product.js
+++ js/product.js
@@ -1,1 +1,280 @@
-function buildBreadCrumb(n){$container=$(".bread-crumb");let t="catalog.html?category="+normalizeText(n.Category);addBreadCrumb($container,n.Category,t);n.SubCategory!=null&&n.SubCategory.length>0&&(t+="&subcategory="+normalizeText(n.SubCategory),addBreadCrumb($container,n.SubCategory,t));addBreadCrumb($container,n.Label)}function addBreadCrumb(n,t,i=null){if(i==null){let i=document.createElement("span");i.setAttribute("class","stext-109 cl4");i.textContent=spanishFormat(t);n.append(i);return}let r=document.createElement("a");r.setAttribute("href",i);r.setAttribute("class","stext-109 cl8 hov-cl1 trans-04");r.textContent=spanishFormat(t);let u=document.createElement("i");u.setAttribute("class","fa fa-angle-right m-l-9 m-r-10");u.setAttribute("aria-hidden","true");r.appendChild(u);n.append(r)}function buildGallery(n){$container=$(".gallery-lb");n.Images.forEach(function(n){addGalleryItem($container,n)});$container.each(function(){$(this).magnificPopup({delegate:"a",type:"image",gallery:{enabled:!0},mainClass:"mfp-fade"})})}function addGalleryItem(n,t){t="."+t;let i=document.createElement("div");i.setAttribute("class","item-slick3");i.setAttribute("data-thumb",t);let r=document.createElement("div");r.setAttribute("class","wrap-pic-w pos-relative");let f=document.createElement("img");f.setAttribute("src",t);f.setAttribute("alt","imagen del art�culo");r.appendChild(f);let u=document.createElement("a");u.setAttribute("class","flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04");u.setAttribute("href",t);let e=document.createElement("i");e.setAttribute("class","fa fa-expand");u.appendChild(e);r.appendChild(u);i.appendChild(r);n.append(i)}function buildRelatedProducts(n){n!=null&&n.Category!=null&&n.Category.length!=0&&n.SubCategory!=null&&n.SubCategory.length!=0&&$.getJSON("./data/products-index.json",function(t){if(t!=null){let r=t[n.Category];if(r!=null){let i=r[n.SubCategory];if(i!=null){i=getCiclon(i,n.Label);$container=$(".slick2");i.forEach(function(t){t.Category=n.Category;t.SubCategory=n.SubCategory;addProductCardBase($container,t,"",2)});$.getScript("./js/slick-custom.js",function(){});var u=new LazyLoad({elements_selector:"img[data-src]"})}}}})}function getCiclon(n,t){const u=[];if(n==null||n.length==0)return u;let r=n.sort((n,t)=>n.Price-t.Price),i=r.findIndex(n=>n.Label===t);if(i<0)return r;if(r.splice(i,1),i+1==n.length)return n.sort((n,t)=>t.Price-n.Price);let f=-1,e=12;while(r.length>0&&e>0)i>=r.length?i=r.length-1:i<0&&(i=0),u.push(r[i]),r.splice(i,1),f==-1&&i--,f*=-1,e--;return u}function addRelatedItem(n,t){let f=document.createElement("div");f.setAttribute("class","item-slick2 p-l-15 p-r-15 p-t-15 p-b-15");let r=document.createElement("div");r.setAttribute("class","block2");let i=document.createElement("div");i.setAttribute("class","block2-pic hov-img0");const s=ToSlug(t.Label);let e=document.createElement("img");e.setAttribute("src","./images/products/"+s+"-0.webp");e.setAttribute("alt","imagen del art�culo");i.appendChild(e);r.appendChild(i);i=document.createElement("div");i.setAttribute("class","block2-txt flex-w flex-t p-t-14");div4Element=document.createElement("div");div4Element.setAttribute("class","block2-txt-child1 flex-col-l ");let u=document.createElement("a");u.setAttribute("href","product.html?id="+s);u.setAttribute("class","stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6");u.innerText=spanishFormat(t.Label);div4Element.appendChild(u);let o=document.createElement("span");o.setAttribute("class","stext-105 cl3");o.innerText=spanishFormat(t.Label);div4Element.appendChild(o);i.appendChild(div4Element);r.appendChild(i);f.appendChild(r);n.append(f)}(function(n){"use strict";n(document).ready(function(){var i=new URLSearchParams(window.location.search),t=i.get("id");n.getJSON("./data/products/"+t+".json",function(i){let r=spanishFormat(i.Label);document.title=r;n("head").append('<meta property="og:title" content="'+spanishFormat(r)+'">');n("head").append('<meta property="og:image" content="./images/products/'+t+'-0.webp">');whatsappMessage='Hola, me interesa el artículo "'+r+'" que vi en tu sitio web. ¿Podrías darme más información?';whatsappMessage=spanishFormat(whatsappMessage);buildBreadCrumb(i);buildGallery(i);var u=!0;n(".product-label").each(function(t,i){i.textContent=r;u&&productStatus!=null&&productStatus.length>0&&(n(i).addClass("pstatus"),n(i).attr("data-pstatus",productStatus),u=!1)});let e=toMoneyStr(i.Price);n(".product-price").each(function(t,r){if(r.textContent=e,i.Discount>0){let t=i.Discount.toLocaleString("en-US",{style:"currency",currency:"USD",minimumFractionDigits:0});n(r).addClass("pdiscount");n(r).attr("data-pdiscount","- "+t)}});let f=spanishFormat(i.Features.join(", "));n("head").append('<meta property="og:description" content="'+f+'">');n(".product-features").each(function(n,t){t.textContent=f});let o=spanishFormat(i.Description);n(".product-description").each(function(n,t){t.innerHTML=o.replace(/\n/g,"<br>")});n(".js-addcart-detail").each(function(n,i){i.setAttribute("product-label",r);i.setAttribute("product-id",t)});buildRelatedProducts(i)})})})(jQuery)
+
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        
+        var urlParams = new URLSearchParams(window.location.search);
+        var productId = urlParams.get('id');
+        $.getJSON('./data/products/' + productId + '.json', function (product) {
+
+            let title = spanishFormat(product.Label);
+            document.title = title;
+          
+            $('head').append('<meta property="og:title" content="' + spanishFormat(title) + '">');
+            $('head').append('<meta property="og:image" content="./images/products/' + productId +'-0.webp">');
+            
+            whatsappMessage = 'Hola, me interesa el art\u00EDculo \"' + title + '\" que vi en tu sitio web. \u00BFPodr\u00EDas darme m\u00E1s informaci\u00F3n?';
+            whatsappMessage = spanishFormat(whatsappMessage);
+
+            buildBreadCrumb(product);
+            buildGallery(product);
+
+            var first = true; 
+            $('.product-label').each(function (index, element) {
+                element.textContent = title;
+
+                if (first && productStatus != null && productStatus.length > 0) {
+                    $(element).addClass("pstatus");
+                    $(element).attr("data-pstatus", productStatus);
+                    first = false;
+                }
+            });
+
+            let currency = toMoneyStr(product.Price);
+            $('.product-price').each(function (index, element) {
+                element.textContent = currency;
+
+                if (product.Discount > 0) {
+                    let discountStr = product.Discount.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 });
+                    $(element).addClass("pdiscount");
+                    $(element).attr("data-pdiscount", '- ' + discountStr);
+                }
+            });
+
+            let featuresStr = spanishFormat(product.Features.join(', '));
+            $('head').append('<meta property="og:description" content="' + featuresStr +'">');
+            $('.product-features').each(function (index, element) {
+                element.textContent = featuresStr;
+            });
+
+            let description = spanishFormat(product.Description);
+            $('.product-description').each(function (index, element) {
+                element.innerHTML = description.replace(/\n/g, '<br>');;
+            });
+
+            $('.js-addcart-detail').each(function (index, element) {                
+                element.setAttribute('product-label', title);
+                element.setAttribute('product-id', productId);                
+            });
+
+            buildRelatedProducts(product);
+        });
+    });
+
+})(jQuery);
+
+function buildBreadCrumb(product) {
+
+    $container = $('.bread-crumb');
+    let href = 'catalog.html?category=' + normalizeText(product.Category);
+    addBreadCrumb($container, product.Category, href);
+
+    if (product.SubCategory != null && product.SubCategory.length > 0) {
+        href += '&subcategory=' + normalizeText(product.SubCategory);
+        addBreadCrumb($container, product.SubCategory, href);
+    }
+    
+    addBreadCrumb($container, product.Label);
+}
+
+function addBreadCrumb($container, label, href = null) {
+    if (href == null) {
+        let spanElement = document.createElement("span");
+        spanElement.setAttribute("class", "stext-109 cl4");
+        spanElement.textContent = spanishFormat(label);
+
+        $container.append(spanElement);
+        return;
+    }
+
+    let aElement = document.createElement("a");
+    aElement.setAttribute("href", href);
+    aElement.setAttribute("class", "stext-109 cl8 hov-cl1 trans-04");
+    aElement.textContent = spanishFormat(label);
+
+    let iElement = document.createElement("i");
+    iElement.setAttribute("class", "fa fa-angle-right m-l-9 m-r-10");
+    iElement.setAttribute("aria-hidden", 'true');
+
+    aElement.appendChild(iElement);
+
+    $container.append(aElement);
+}
+
+function buildGallery(product) {
+
+    $container = $('.gallery-lb');
+
+    product.Images.forEach(function (item) {
+        addGalleryItem($container, item);
+    });
+
+    $container.each(function () { // the containers for all your galleries
+        $(this).magnificPopup({
+            delegate: 'a', // the selector for gallery item
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+            mainClass: 'mfp-fade'
+        });
+    });
+}
+
+function addGalleryItem($container, src) {
+
+    src = "." + src;
+
+    let divElement = document.createElement("div");    
+    divElement.setAttribute("class", "item-slick3");
+    divElement.setAttribute("data-thumb", src);   
+
+    let div2Element = document.createElement("div");
+    div2Element.setAttribute("class", "wrap-pic-w pos-relative");
+
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", src);
+    imgElement.setAttribute("alt", 'imagen del art�culo');
+
+    div2Element.appendChild(imgElement);
+
+    let aElement = document.createElement("a");
+    aElement.setAttribute("class", 'flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04');
+    aElement.setAttribute("href", src);
+
+    let iElement = document.createElement("i");
+    iElement.setAttribute("class", 'fa fa-expand');
+
+    aElement.appendChild(iElement);
+
+    div2Element.appendChild(aElement);
+
+    divElement.appendChild(div2Element);
+
+    $container.append(divElement);
+}
+
+function buildRelatedProducts(product) {
+
+    if (product == null || product.Category == null || product.Category.length == 0 || product.SubCategory == null || product.SubCategory.length == 0)
+        return;
+            
+    $.getJSON("./data/products-index.json", function (data) {
+
+        if (data == null)
+            return;
+
+        let subcategories = data[product.Category];
+        if (subcategories == null)
+            return;
+
+        let products = subcategories[product.SubCategory];
+        if (products == null)
+            return;
+
+        products = getCiclon(products, product.Label);
+        $container = $('.slick2');        
+        products.forEach(function (item) {
+            item.Category = product.Category;
+            item.SubCategory = product.SubCategory;
+            addProductCardBase($container, item, '', 2);    
+        });
+
+        $.getScript('./js/slick-custom.js', function () {
+            
+        });
+
+        var lazyLoadInstance = new LazyLoad({
+            elements_selector: "img[data-src]"            
+        });
+    });
+}
+
+function getCiclon(products, label) {
+
+    const newProducts = [];
+    if (products == null || products.length == 0)
+        return newProducts;
+
+    let orderProducts = products.sort((a, b) => a.Price - b.Price);
+    let index = orderProducts.findIndex(product => product.Label === label);
+
+    if (index < 0)
+        return orderProducts;
+
+    orderProducts.splice(index, 1);   
+
+    if (index + 1 == products.length)
+        return products.sort((a, b) => b.Price - a.Price);
+
+    let sign = -1;    
+     
+    let count = 12; 
+    while (orderProducts.length > 0 && count > 0) {
+
+        if (index >= orderProducts.length)
+            index = orderProducts.length - 1;
+        else if (index < 0)
+            index = 0;
+
+        newProducts.push(orderProducts[index]);
+        orderProducts.splice(index, 1);
+        
+        if (sign == -1)
+            index--;
+
+        sign *= -1;
+        count--;
+    }
+
+    return newProducts;
+}
+
+function addRelatedItem($container, item) {
+    let divElement = document.createElement("div");
+    divElement.setAttribute("class", "item-slick2 p-l-15 p-r-15 p-t-15 p-b-15");
+
+    let div2Element = document.createElement("div");
+    div2Element.setAttribute("class", "block2");
+
+    let div3Element = document.createElement("div");
+    div3Element.setAttribute("class", "block2-pic hov-img0");
+
+    const slug = ToSlug(item.Label);
+
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", "./images/products/" + slug + "-0.webp");
+    imgElement.setAttribute("alt", 'imagen del art�culo');
+
+    div3Element.appendChild(imgElement);
+
+    div2Element.appendChild(div3Element);
+
+    div3Element = document.createElement("div");
+    div3Element.setAttribute("class", "block2-txt flex-w flex-t p-t-14");
+
+    div4Element = document.createElement("div");
+    div4Element.setAttribute("class", "block2-txt-child1 flex-col-l ");
+
+    let aElement = document.createElement("a");
+    aElement.setAttribute("href", "product.html?id=" + slug);
+    aElement.setAttribute("class", "stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6");
+    aElement.innerText = spanishFormat(item.Label);
+
+    div4Element.appendChild(aElement);
+
+    let spanElement = document.createElement("span");
+    spanElement.setAttribute("class", "stext-105 cl3");
+    spanElement.innerText = spanishFormat(item.Label);
+
+    div4Element.appendChild(spanElement);
+
+    div3Element.appendChild(div4Element);
+
+    div2Element.appendChild(div3Element);
+
+    divElement.appendChild(div2Element);
+
+    $container.append(divElement);
+}
